8322d9ea740f821d9ac3f788fe05df15cf9b67de
Konrad Kasperczyk <kkasperczyk20@gmail.com>
2020-06-30 18:41:07 +0200

    added styling to 'tasks' + basic js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,86 @@
     console.log("Hi all developers! Nice to see you here. Enjoy!");
   };
 
+  const tasks = [];
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+
+    render();
+  };
+
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+
+    render();
+  };
+
+  const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
+    });
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+      });
+    });
+  };
+
+  const render = () => {
+    let htmlString = "";
+
+    for (task of tasks) {
+      htmlString += `<li class="js-tasks__list-item" ${
+        task.done ? 'style="text-decoration: line-through"' : ""
+      }>
+                        <button class="js-done"></button>
+                        <span class="js-taskContent">${task.content}</span>
+                        <button class="js-remove">🗑</button>
+                    </li>
+    `;
+    }
+
+    document.querySelector(".js-tasks__list").innerHTML = htmlString;
+
+    bindEvents();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document
+      .querySelector(".js-form__input")
+      .value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  };
+
   const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+
     welcome();
   };
 
